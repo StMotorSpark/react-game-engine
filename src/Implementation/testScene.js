3b396f04d2760f063e6eea43032d39847f40498f
Ben Simons <robot13ds@gmail.com>
2022-11-14 04:15:11 +0000

    Testing out some of the environment stuff related to the new Github Codespace stuff

--- src/Implementation/testScene.js
+++ src/Implementation/testScene.js
@@ -1,15 +1,33 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 // import { SceneContext } from "../engine/global/components/SceneManager";
 import Scene from "../engine/global/components/Scene";
 import Node from "../engine/node/components/Node";
+import { SceneContext } from "../engine/global/components/SceneManager";
 
-function TestScene() {
+function useTestSceneSate() {
+    const { changeScene } = useContext(SceneContext);
+
+    const goToNextLevel = () => {
+        console.log("go to next level");
+        changeScene("scene1");
+    }
+
+    return {
+        goToNextLevel
+    }
+}
+
+function TestScene(props) {
+    const { goToNextLevel } = useTestSceneSate(props);
 
     return (
         <Scene>
             <Node />
             <Node />
+            <button onClick={goToNextLevel}>
+                Next Level
+            </button>
         </Scene>
     );
 }
